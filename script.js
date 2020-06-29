14afed2f5009fd63b034c7f79bb3544ff2a2d4fe
Pablo Cianca <pcianca@protonmail.com>
2020-06-29 23:00:47 +0000

    cambio de cursor

--- script.js
+++ script.js
@@ -11,4 +11,22 @@ $('#evento').sheetrock({
   rowTemplate: template
 });
 
-// arreglar headers y cambiar por labels
+(function(){
+  _386.magicCursor();
+  _386.scrollLock();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+
+  $(window).scroll(function() {
+    var top = $(document).scrollTop();
+    if(top > 50)
+      $('#home > .navbar').removeClass('navbar-transparent');
+    else
+      $('#home > .navbar').addClass('navbar-transparent');
+  });
+
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
+
+})();
